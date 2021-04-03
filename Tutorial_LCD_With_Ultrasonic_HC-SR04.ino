e091f0c07d994f26dace032ab34edfc650ef8fb3
GedeWekayana <81853622+GedeWekayana@users.noreply.github.com>
2021-04-03 17:31:04 +0800

    Add files via upload

---
+++ Tutorial_LCD_With_Ultrasonic_HC-SR04.ino
@@ -0,0 +1,38 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distanceCm, distanceInch;
+
+void setup() {
+  // put your setup code here, to run once:
+lcd.begin();
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+distanceInch = duration*0.0133/2;
+
+lcd.setCursor(0,0);
+lcd.print("Distance: ");
+lcd.print(distanceCm);
+lcd.print("  cm");
+delay(500);
+lcd.setCursor(0,1);
+lcd.print("Distance: ");
+lcd.print(distanceInch);
+lcd.print(" inch");
+delay(500);
+}
